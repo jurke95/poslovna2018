93e74a7993cec499e65be13e3fe2278e3a882e61
aleksandragr <grujica995@gmail.com>
2018-07-05 01:15:59 +0200

    legal entity fix

--- poslovna/src/main/java/com/poslovna/controller/LegalEntityController.java
+++ poslovna/src/main/java/com/poslovna/controller/LegalEntityController.java
@@ -1,9 +1,14 @@
 package com.poslovna.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +28,38 @@ public class LegalEntityController {
 	@PostMapping("/addLegEntity")
 	public ResponseEntity<LegalEntity> addNewLegalEntity(@RequestBody LegalEntity legEnt){
 		
-		LegalEntity legalEntity = null;
+		LegalEntity legalEntity = legalEntityService.addLegalEntity(legEnt);
 		
 		if(legalEntity==null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		
 		return new ResponseEntity<>(legalEntity, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteLegEnt/{id}")
+	public ResponseEntity<LegalEntity> deleteLegalEnt(@PathVariable Long id){
+		
+		LegalEntity legEnt = legalEntityService.deleteLegalEntity(id);
+		
+		if(legEnt==null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<>(legEnt, HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("/getAllLegEntities")
+	public ResponseEntity<List<LegalEntity>> findAllLegalEntity(){
+		
+		List<LegalEntity> legEntities = legalEntityService.findAllLenEnt();
+		
+		if(legEntities==null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<>(legEntities, HttpStatus.OK);
 	}
 
 }
