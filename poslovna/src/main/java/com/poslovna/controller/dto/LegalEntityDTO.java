0dd8d3dc03b2ba539b272e40affae9a26cab392a
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-07-11 15:24:27 +0200

    delete and edit legal entity

---
+++ poslovna/src/main/java/com/poslovna/controller/dto/LegalEntityDTO.java
@@ -0,0 +1,182 @@
+package com.poslovna.controller.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
+import com.poslovna.model.Activity;
+import com.poslovna.model.Bank;
+
+public class LegalEntityDTO {
+	
+	public LegalEntityDTO() {
+		
+		
+	}
+	
+	private Long id;
+	
+	private String name;
+	
+	private String shortName;
+	
+	private String location;
+	
+	private String address;
+	
+	private String phonenumber;
+	
+	private String fax;
+	
+	private String email;
+	
+	private String responsiblePerson;
+	
+	private Activity activity;
+	
+	private String mbr;
+	
+	private String jmbg;
+	
+	private String taxAuthority;
+	
+	private String taxNumber;
+	
+	private Long bankid;
+	
+	private String deliveryAddress;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getResponsiblePerson() {
+		return responsiblePerson;
+	}
+
+	public void setResponsiblePerson(String responsiblePerson) {
+		this.responsiblePerson = responsiblePerson;
+	}
+
+	public Activity getActivity() {
+		return activity;
+	}
+
+	public void setActivity(Activity activity) {
+		this.activity = activity;
+	}
+
+	public String getMbr() {
+		return mbr;
+	}
+
+	public void setMbr(String mbr) {
+		this.mbr = mbr;
+	}
+
+	public String getJmbg() {
+		return jmbg;
+	}
+
+	public void setJmbg(String jmbg) {
+		this.jmbg = jmbg;
+	}
+
+	public String getTaxAuthority() {
+		return taxAuthority;
+	}
+
+	public void setTaxAuthority(String taxAuthority) {
+		this.taxAuthority = taxAuthority;
+	}
+
+	public String getTaxNumber() {
+		return taxNumber;
+	}
+
+	public void setTaxNumber(String taxNumber) {
+		this.taxNumber = taxNumber;
+	}
+
+	public Long getBankid() {
+		return bankid;
+	}
+
+	public void setBankid(Long bankid) {
+		this.bankid = bankid;
+	}
+
+	public String getDeliveryAddress() {
+		return deliveryAddress;
+	}
+
+	public void setDeliveryAddress(String deliveryAddress) {
+		this.deliveryAddress = deliveryAddress;
+	}
+	
+	
+}
