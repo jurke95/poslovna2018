7cfa82badac550ebd9592396dde421785db0e624
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-07-14 03:50:47 +0200

    Account closure fix

--- poslovna/src/main/java/com/poslovna/service/AccountClosureService.java
+++ poslovna/src/main/java/com/poslovna/service/AccountClosureService.java
@@ -1,5 +1,6 @@
 package com.poslovna.service;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -51,35 +52,81 @@ public class AccountClosureService {
 		
 		
 		Account debtorAccount = accountRepository.findOneByAccountnum(accdto.getAccountfrom());
-		Account creditorAccount = accountRepository.findOneByAccountnum(accdto.getAccountfrom());
+		
+		Account creditorAccount = accountRepository.findOneByAccountnum(accdto.getAccountto());
 		
 		List<DailyAccountBalance> dailyAccountBalansdebtor = dabRepository.findByBankaccount_id(debtorAccount.getId());
 		List<DailyAccountBalance> dailyAccountBalanscreditor = dabRepository.findByBankaccount_id(creditorAccount.getId());
 
 		
+		
+		
+		
+		
+		DailyAccountBalance maxx = new DailyAccountBalance();
 		//onome kome se skida
 		if(dailyAccountBalansdebtor.size()!=0) {
 			
-			DailyAccountBalance max = dailyAccountBalansdebtor.get(0);
+			maxx = dailyAccountBalansdebtor.get(0);
 			SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
 			java.util.Date maxDate = date.parse(dailyAccountBalansdebtor.get(0).getDate());
 			//java.util.Date fromDate = date.parse(sa.getDateCurrency());
 
-			for (int i = 1; i < dailyAccountBalansdebtor.size(); i++) {
+			for (int i = 0; i < dailyAccountBalansdebtor.size(); i++) {
 				java.util.Date currentDate = date.parse(dailyAccountBalansdebtor.get(i).getDate());
 
 				if (currentDate.after(maxDate)) {
-					max = dailyAccountBalansdebtor.get(i);
+					maxx = dailyAccountBalansdebtor.get(i);
 				}
 			}
 			
-			System.out.println("AAAAAAAAAAaa " + max.getBankaccount().getAccountnum());
+	 
+		//onome kome se prebacuje
+				if(dailyAccountBalanscreditor.size()!=0) {
+					
+					DailyAccountBalance primac = dailyAccountBalanscreditor.get(0);
+					SimpleDateFormat datee = new SimpleDateFormat("yyyy-MM-dd");
+					java.util.Date maxDatee = datee.parse(dailyAccountBalanscreditor.get(0).getDate());
+					//java.util.Date fromDate = date.parse(sa.getDateCurrency());
+
+					for (int i = 0; i < dailyAccountBalanscreditor.size(); i++) {
+						java.util.Date currentDate = date.parse(dailyAccountBalanscreditor.get(i).getDate());
+						System.out.println("EEEEEEEEEE " + dailyAccountBalanscreditor.get(i).getNewState());
+						if (currentDate.after(maxDatee)) {
+							primac = dailyAccountBalanscreditor.get(i);
+						}
+					}
+					
+					DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+					Date today = new Date();      
+					String reportDate = df.format(today);
+					
+					
+					
+					primac.setPaymentFrom(0.0);
+					primac.setPaymentTo(0.0);
+					//max.setPreviousState(0.0);
+					
+					primac.setPaymentTo(maxx.getNewState());
 			
+					primac.setDate(reportDate);
+					
+					primac.setNewState(primac.getNewState() + primac.getPaymentTo() - primac.getPaymentFrom());
+					dabRepository.save(primac);
+					
+					
+					
+					
+					maxx.setNewState(0.0);
+					maxx.setDate(reportDate);
+					maxx.setPaymentFrom(0.0);
+					maxx.setPaymentTo(0.0);
+					maxx.setPreviousState(0.0);
+					dabRepository.save(maxx);
+				}
+		
+		
 		}
-		
-		
-		
-		
 		
 		
 		
